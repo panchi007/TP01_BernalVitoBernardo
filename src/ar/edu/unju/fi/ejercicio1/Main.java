e61ff4915fa2345c5bfa91d1e1b240ea48c6bd9c
panchi007 <vitobernal@hotmail.com>
2024-04-05 18:17:38 -0300

    Ejercicio 1

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num1, num2, num3, num4, num5;
+        
+        // Asignación de valores numéricos a las variables
+        num1 = 8;
+        num2 = 5;
+        num3 = 1;
+        num4 = 20;
+        num5 = 9;
+        
+        // Cálculo del promedio
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+        
+        // Mostrar el resultado por consola
+        System.out.println("El promedio es: " + promedio);
+
+	}
+
+}
