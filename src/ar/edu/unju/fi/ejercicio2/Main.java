c86335ab89a5fd90c37b1902d67b3fa587a359ee
panchi007 <vitobernal@hotmail.com>
2024-04-05 18:22:42 -0300

    Ejercicio 2

---
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declaración de variables
+        String nombrePais = "España";
+        int edadPersona = 30;
+        double alturaEdificio = 150.5;
+        double precioProducto = 2.99;
+        String numeroTelefono = "123456789";
+        double coseno = Math.cos(0.5);
+        
+        // Mostrar las variables por consola
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Edad de la persona: " + edadPersona);
+        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio del producto: $" + precioProducto);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Coseno de 0.5: " + coseno);
+
+	}
+
+}
